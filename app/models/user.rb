32cbf8cc190dc6ff5630d86737c20e46e7a73123
pratibha <pratibha.praveen@spa-systems.com>
2015-02-10 11:57:58 +0530

    devise implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable
+  
+  validates :email, :presence => true, :uniqueness => true
+  validates :password, :presence => true, :confirmation => true, :on => :create
 
 	def is_super_admin?
 		return !!(self.role == 'super_admin')

9eadcbdb3bacbb56c4679232c38ce445f7d1a882
pratibha <pratibha.praveen@spa-systems.com>
2015-04-15 19:04:42 +0530

    json string

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -9,30 +9,49 @@ class Api::V1::DashboardsController < ApplicationController
 		if params[:app_user_id].present?
 			@app_user = AppUser.find_by_id(params[:app_user_id])
 		  @service_preferences = @app_user.service_preferences if @app_user.present?
-			@service_preferences.each do |sp|
-				@sc_id = sp.service_category_id
-				@advertisement = Advertisement.where("service_category_id = ?", @sc_id).first
-				@advertisements << @advertisement
-				if sp.service_provider.is_preferred == true
-					@sp_id = sp.service_provider.id
-					@p_deal = Deal.where("is_active = ?", true).where("service_category_id = ?", @sc_id).where("service_provider_id = ?", @sp_id).order("price ASC").first
-					@preferred_deals << @p_deal
-				elsif sp.service_provider.is_preferred == false
-					@sc_deal = Deal.where("is_active = ?", true).where("service_category_id = ?", @sc_id).order("price ASC").first
+		  @servicelist = @service_preferences.map do |sp|
+
+		  	#if sp.service_provider.is_preferred == true
+		  	#	byebug
+		  	#	@p_deal = Deal.where("is_active = ?", true).where("service_category_id = ?", sp.service_category.id).where("service_provider_id = ?", sp.service_provider.id).order("price ASC").first
+		  		#{ :service_preference_id => sp.id, :contract_fee => sp.contract_fee, :service_category_name => sp.service_category.name, :deal => @p_deal.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]) }
+				#elsif sp.service_provider.is_preferred == false
+					#byebug
+				#	@b_deal = Deal.where("is_active = ?", true).where("service_category_id = ?", sp.service_category.id).where("service_provider_id = ?", sp.service_provider.id).order("price ASC").first
+					#{ :service_preference_id => sp.id, :contract_fee => sp.contract_fee, :service_category_name => sp.service_category.name, :deal => @b_deal }
+				#end 
+
+				{ :service_preference_id => sp.id, :contract_fee => sp.contract_fee, :service_category_name => sp.service_category.name } 
+		  end	
+		  #json = @servicelist.to_json
+		  #*************** Working ***********************
+			#@service_preferences.each do |sp|
+			#	@sc_id = sp.service_category_id
+			#	@advertisement = Advertisement.where("service_category_id = ?", @sc_id).first
+			#	@advertisements << @advertisement
+			#	if sp.service_provider.is_preferred == true
+			#		@sp_id = sp.service_provider.id
+			#		@p_deal = Deal.where("is_active = ?", true).where("service_category_id = ?", @sc_id).where("service_provider_id = ?", @sp_id).order("price ASC").first
+			#		@preferred_deals << @p_deal
+			#	elsif sp.service_provider.is_preferred == false
+			#		@sc_deal = Deal.where("is_active = ?", true).where("service_category_id = ?", @sc_id).order("price ASC").first
 					#unless @deals.include?(@sc_deal)
-						@deals << @sc_deal
+			#			@deals << @sc_deal
 					#end	
-				end	
-			end	
-			render :json => { 
+			#	end	
+			#end
+			#****************** up to here ***************	
+			render :json => @servicelist
 												#:app_user => @app_user.as_json(:include => { :service_preferences => { :include => { :service_category => { :include => { :deals => { :only => [:id, :title]}}, :only => [:id, :name]}}, :only => [:service_category_id, :service_provider_id, :contract_fee]}}),
 												#:service_preference => @service_preferences.as_json(:include => { :service_category => { :include => :deals, :include => :advertisement }})
-												#********** Working **************
-												:service_preference => @service_preferences.as_json(:include => { :service_category => {:only => [:id, :name]}}, :only => [:id, :contract_fee] ),
-												:deal => @deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]),
-												:preferred_deal => @preferred_deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]),
-		                		:advertisement => @advertisements.as_json(:methods => [:ad_image_url])
-		                	}
+												#****************** Testing ********************
+												#:service_preference => @service_preferences.as_json(:include => { :service_category => {:only => [:id, :name]}}, :only => [:id, :contract_fee] )
+												#************ Working till date **************
+												#:service_preference => @service_preferences.as_json(:include => { :service_category => {:only => [:id, :name]}}, :only => [:id, :contract_fee] ),
+												#:deal => @deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]),
+												#:preferred_deal => @preferred_deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]),
+		                		#:advertisement => @advertisements.as_json(:methods => [:ad_image_url])
+		                	
 		elsif params[:zip_code].present? && params[:category].blank? 
 			@service_categories = ServiceCategory.all
 			@service_preference = []

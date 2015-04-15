7a8f5f2abf366c448e8d9b5c89850c6f4862cd0b
pratibha <pratibha.praveen@spa-systems.com>
2015-04-15 12:13:02 +0530

    deal price

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -55,69 +55,20 @@ class Api::V1::DashboardsController < ApplicationController
 			end	
 			render :json => { 
 												:service_preference => @service_preference, 
-												:deal => @deals.as_json(:methods => [:deal_image_url]),
-												:preferred_deal => @preferred_deals.as_json(:methods => [:deal_image_url]),
+												:deal => @deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]),
+												:preferred_deal => @preferred_deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :deal_price]),
 												:advertisement => @advertisements.as_json(:methods => [:ad_image_url])
 											}
 		elsif params[:category].present? && params[:zip_code].blank?
 			@deals = Deal.where("is_active = ?", true).where(service_category_id: params[:category]).order("price ASC")	              
 			render :json => {
-												:deal => @deals.as_json(:methods => [:deal_image_url, :average_rating, :rating_count])
+												:deal => @deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :average_rating, :rating_count, :deal_price])
 											}
 		elsif params[:zip_code].present? && params[:category].present? 
 			@deals = Deal.where("is_active = ?", true).where(zip: params[:zip_code]).where(service_category_id: params[:category]).order("price ASC")
 			render :json => {
-												:deal => @deals.as_json(:methods => [:deal_image_url, :average_rating, :rating_count])
+												:deal => @deals.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :average_rating, :rating_count])
 											}
 		end                
-			#render :json => @app_user, :include => { :service_preferences => { :include => { :service_category  => { :only => [:id, :name] }}, :only => :service_category_id }}
-			#render :json => @app_user, :include => { :service_preferences => {
-			#																																		:include => { :service_category => { :only => [:id, :name] } },
-			#																																		:only => [:service_category_id, :service_provider_id, :contract_date, :is_contract, :contract_fee]
-			#																																	} 
-			#																				}
-			#render :json => @app_user, :include => { :service_preferences => { :include => { :service_category => { :include => {:deals => { :only => :title }}, { :only => [:id, :name] } }, :only => [:service_category_id, :service_provider_id]} }}
-			#@app_user = AppUser.find_by_id(params[:app_user_id]) 
-			#@service_preference = ServicePreference.where("app_user_id = ?", params[:app_user_id])
-			#@app_user = AppUser.joins(:service_preferences).where("app_user_id = ?", params[:app_user_id])
-			#@service_preference = @app_user.map{|c| c.service_preferences}
-			#render :json => { :service_preference => @service_preferences }
-			#@service_preference.each do |variable|
-			#	if variable.service_category_name == "Internet"
-			#		@internet_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Internet').order("price DESC")
-			#	elsif variable.service_category_name == "Telephone"
-			#		@telephone_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Telephone').order("price DESC")
-			#	elsif variable.service_category_name == "Cable"
-			#		@cable_deal = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Cable').order("price DESC")
-			#	elsif variable.service_category_name == "Cellphone"	
-			#		@cellphone_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Cellphone').order("price DESC")
-			#	elsif variable.service_category_name == "Gas"
-			#		@gas_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Gas').order("price DESC")
-			#	elsif variable.service_category_name == "Electricity"
-			#		@electricity_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Electricity').order("price DESC")
-			#	elsif variable.service_category_name == "Bundle"
-			#		@bundle_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Bundle').order("price DESC")
-			#	elsif variable.service_category_name == "Home Security"	
-			#		@hs_deals = Deal.where("is_active = ?", true).where("service_category_name = ?", 'Home Security').order("price DESC")
-			#	end
-			#end
-			#if @app_user.present?
-			#	render :status => 200,
-    	#       	 :json => { :success            => true, 
-    	#       	 						:app_user           => @app_user, 
-    	#       	 						:service_preference => @service_preference,
-    	#       	 						:internet_deals     => @internet_deals,
-    	#       	 						:telephone_deals    => @telephone_deals,
-    	#       	 						:cable_deals        => @cable_deals,
-    	#       	 						:cellphone_deals    => @cellphone_deals,
-    	#       	 						:gas_deals          => @gas_deals,
-	    #       	 						:electricity_deals  => @electricity_deals,
-    	#       	 						:bundle_deals       => @bundle_deals,
-    	#       	 						:hs_deals           => @hs_deals,
-    	#       	 					}
-    	#else
-    	#  render :status => 401,
-    	#       	 :json => { :success => false }
-    	#end
 	end
 end

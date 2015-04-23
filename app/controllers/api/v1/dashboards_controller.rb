3c2e4b6246a74008bff59ec94735e64db8b4d251
pratibha <pratibha.praveen@spa-systems.com>
2015-04-23 15:01:25 +0530

    error fix

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -2,31 +2,8 @@ class Api::V1::DashboardsController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	respond_to :json
 
-	def index
-		if params[:app_user_id].present? && params[:zip_code].blank? && params[:category].blank?
-			@app_user = AppUser.find_by_id(params[:app_user_id])
-		  @service_preferences = @app_user.service_preferences if @app_user.present?
-		  @servicelist = @service_preferences.map do |sp|
-		  	@advertisement = []
-		  	@best_deal = []
-		  	@b_deal = Deal.where("is_active = ? AND service_category_id = ?", true, sp.service_category_id).order("price ASC").first
-		  	@best_deal << @b_deal if @b_deal.present?
-		  	@adv = sp.service_category.advertisements.order("created_at DESC").first
-		  	@advertisement << @adv if @adv.present?
-		  	sp.service_category.service_providers.map do |pp|
-		  		if pp.is_preferred == true
-		  			@preferred_deal = []
-		  			@p_deal = Deal.where("is_active = ? AND service_category_id = ? AND service_provider_id = ?", true, pp.service_category_id, pp.id).order("price ASC").first
-						@preferred_deal << @p_deal
-					else
-						@preferred_deal = []
-		  		end
-		  	end	
-				{ :contract_fee => sp.contract_fee, :service_category_name => sp.service_category.name, :advertisement => @advertisement.as_json(:except => [:created_at, :updated_at, :image], :methods => [:advertisement_image_url]), :best_deal => @best_deal.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :average_rating, :rating_count, :deal_price]), :preferred_deal => @preferred_deal.as_json(:except => [:created_at, :updated_at, :price, :image], :methods => [:deal_image_url, :average_rating, :rating_count, :deal_price]) } 
-		  end	
-			render :json => { :dashboard_data => @servicelist }
-													                	
-		elsif params[:zip_code].present? && params[:category].blank? && params[:app_user_id].blank?
+	def index													                	
+		if params[:zip_code].present? && params[:category].blank? && params[:app_user_id].blank?
 			@service_categories = ServiceCategory.all
 			@servicelist = @service_categories.map do |sc|
 				@advertisement = []

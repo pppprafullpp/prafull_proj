75f790e1aacf5cdb6d59ad2fa7425878b95fac2b
pratibha <pratibha.praveen@spa-systems.com>
2015-04-24 16:13:27 +0530

    test

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -35,11 +35,12 @@ class Api::V1::DashboardsController < ApplicationController
 				@advertisement << @adv if @adv.present?
 				@service_providers = sc.service_providers
 				@service_providers.map do |pp|
+					@p_id = pp.id
 					@preferred_deal = []
 					if pp.is_preferred == false
 						@best_deal = []
 						#@b_deal = "best deal"
-						@b_deal = Deal.where("is_active = ? AND zip = ? AND service_category_id = ? AND service_provider_id = ?", true, params[:zip_code], sc.id, pp.id).first
+						@b_deal = Deal.where("is_active = ? AND zip = ? AND service_category_id = ? AND service_provider_id = ?", true, params[:zip_code], sc.id, @p_id).first
 						@best_deal << @b_deal
 						#@best_deal = []	
 						#@b_deal = Deal.where("is_active = ? AND zip = ? AND service_category_id = ? AND service_provider_id = ?", true, params[:zip_code], sc.id, pp.id).order("price ASC").first

7db87adab7bf092e845ae59f09615a75cb54055a
pratibha <pratibha.praveen@spa-systems.com>
2015-07-21 17:41:24 +0530

    capistrano setup

--- Gemfile
+++ Gemfile
@@ -4,9 +4,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
-#gem 'sqlite3'
-#gem 'mysql2'
-gem 'pg'
+gem 'sqlite3'
+gem 'mysql2'
+#gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -35,6 +35,13 @@ gem "rmagick"
 gem "carrierwave"
 gem 'cloudinary'
 gem 'rails_12factor', group: :production
+gem 'gcm'
+
+group :development do
+	gem 'capistrano', '~> 3.1.0'
+	gem 'capistrano-bundler', '~> 1.1.2'
+	gem 'capistrano-rails', '~> 1.1.1'
+end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+# config valid only for Capistrano 3.1
+set :application, 'service_deals'
+set :repo_url, 'https://ramgarg:@github.com/ramgarg/Service-Deals-Rails'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/dev/service_deals'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :rvm_ruby_version, '2.2.2'
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+SSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
+
+set :bundle_binstubs, nil
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end

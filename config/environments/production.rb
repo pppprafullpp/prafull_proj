b4ea9ff76e5dfef208173664cb4e879affb9124c
pratibha <pratibha.praveen@spa-systems.com>
2015-08-21 17:17:20 +0530

    smtp changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'http://125.99.186.38' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :user_name            => "proteqtor@sp-assurance.com",
+     :password             => "production@watchdog",
+     :authentication       => "plain",
+     :enable_starttls_auto => true
+  }
 end

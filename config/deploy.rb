f09bfda0aea392613710c37892565d1a33301d44
pratibha <pratibha.praveen@spa-systems.com>
2015-08-28 13:11:19 +0530

    rake task

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,3 +29,12 @@ namespace :deploy do
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
+
+#after "deploy:symlink", "deploy:update_crontab"
+
+#namespace :deploy do
+#  desc "Update the crontab file"
+#  task :update_crontab, :roles => :db do
+#    run "cd #{release_path} && whenever --update-crontab #{application}"
+#  end
+#end
--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,7 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+every :hour do
+	rake "reminder_notification:test_task"
+end	

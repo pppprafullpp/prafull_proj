219794195f53d26d5c2c8ebb82ba822e0ab7a286
pratibha <pratibha.praveen@spa-systems.com>
2015-10-26 12:57:00 +0530

    deal notification

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -19,6 +19,7 @@ class DealsController < ApplicationController
 		@deal = Deal.new(deal_params)    
     respond_to do |format|
       if @deal.save
+        send_notification
         format.html { redirect_to deals_path, :notice => 'You have successfully created a deal' }
         format.xml  { render :xml => @deal, :status => :created, :deal => @deal }
       else
@@ -54,6 +55,16 @@ class DealsController < ApplicationController
   end
   
 	private
+
+  def send_notification
+    @app_user = AppUser.where("zip = ?",params[:deal][:zip])
+    gcm = GCM.new("AIzaSyASkbVZHnrSGtqjruBalX0o0rQRA1dYU7w")
+    @app_user.map do |a_user|
+      registration_id = ["#{a_user.gcm_id}"]
+      #message = "New deal for zip #{params[:deal][:zip]}.Visit Url : #{params[:deal][:url]}"
+      gcm.send(registration_id, {data: {message: "New deal for zip #{params[:deal][:zip]}.Visit Url : #{params[:deal][:url]}"}})
+    end  
+  end
 
   def deal_params
   	params.require(:deal).permit(:service_category_id, :service_provider_id, :service_category_name, :service_provider_name, :title, :state, :city, :zip, :short_description, :detail_description, :price, :url, :you_save_text, :start_date, :end_date, :image, :is_active)

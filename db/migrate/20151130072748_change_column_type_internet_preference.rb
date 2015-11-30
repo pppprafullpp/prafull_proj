0375571e1ea80fd3f7467ce88b50e4700467ff4a
pratibha <pratibha.praveen@spa-systems.com>
2015-11-30 18:19:10 +0530

    migration error fix

--- db/migrate/20151130072748_change_column_type_internet_preference.rb
+++ db/migrate/20151130072748_change_column_type_internet_preference.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypeInternetPreference < ActiveRecord::Migration
   def change
-  	change_column :internet_service_preferences, :upload_speed,  :integer
-  	change_column :internet_service_preferences, :download_speed,  :integer
+  	change_column :internet_service_preferences, :upload_speed,  :integer, :default => 0
+  	change_column :internet_service_preferences, :download_speed,  :integer, :default => 0
   end
 end

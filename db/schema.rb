568092ce734c63e50efdadf3a203a64fa177b6f5
pratibha <pratibha.praveen@spa-systems.com>
2015-12-01 12:32:52 +0530

    migration issue fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201051546) do
+ActiveRecord::Schema.define(version: 20151201065816) do
 
   create_table "advertisements", force: :cascade do |t|
     t.integer  "service_category_id",   limit: 4
@@ -58,19 +58,19 @@ ActiveRecord::Schema.define(version: 20151201051546) do
 
   create_table "bundle_service_preferences", force: :cascade do |t|
     t.integer  "service_preference_id", limit: 4
-    t.string   "upload_speed",          limit: 255
-    t.string   "download_speed",        limit: 255
-    t.string   "data",                  limit: 255
     t.integer  "free_channels",         limit: 4
     t.integer  "premium_channels",      limit: 4
     t.integer  "call_minutes",          limit: 4
     t.integer  "text_messages",         limit: 4
     t.integer  "data_plan",             limit: 4
     t.integer  "data_speed",            limit: 4
-    t.boolean  "talk_unlimited",        limit: 1,   default: false
-    t.boolean  "text_unlimited",        limit: 1,   default: false
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.boolean  "talk_unlimited",        limit: 1, default: false
+    t.boolean  "text_unlimited",        limit: 1, default: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "upload_speed",          limit: 4
+    t.integer  "download_speed",        limit: 4
+    t.integer  "data",                  limit: 4
   end
 
   add_index "bundle_service_preferences", ["service_preference_id"], name: "index_bundle_service_preferences_on_service_preference_id", using: :btree

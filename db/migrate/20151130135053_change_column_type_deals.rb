afa94333e43f6e159b4c47e91c2a421be5157f67
pratibha <pratibha.praveen@spa-systems.com>
2015-12-01 10:50:11 +0530

    deal sorting issue fix

--- db/migrate/20151130135053_change_column_type_deals.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnTypeDeals < ActiveRecord::Migration
-  def change
-  	change_column :deals, :upload_speed,  :integer, :default => 0
-  	change_column :deals, :download_speed,  :integer, :default => 0
-  end
-end

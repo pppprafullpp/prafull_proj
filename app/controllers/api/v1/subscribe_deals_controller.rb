bf456891ad052e78fb81416e52f1e128dcea08e3
pratibha <pratibha.praveen@spa-systems.com>
2016-01-06 16:54:43 +0530

    activate issue fix

--- app/controllers/api/v1/subscribe_deals_controller.rb
+++ app/controllers/api/v1/subscribe_deals_controller.rb
@@ -4,57 +4,58 @@ class Api::V1::SubscribeDealsController < ApplicationController
   respond_to :json
 
   def subscription_info
-  	if params[:app_user_id].present? && params[:deal_id].present?
-  		@deal = Deal.find_by_id(params[:deal_id])
-  		@trending_deal = TrendingDeal.where("deal_id =?", params[:deal_id]).take
-  		if @trending_deal.present?
-  			@trending_deal.subscription_count = @trending_deal.subscription_count + 1
-  			@trending_deal.save!
-  		else
-  			@trending_deal = TrendingDeal.new(trending_deal_params)
-  			@trending_deal.category_id = @deal.service_category_id
-  			@trending_deal.subscription_count = 1
-  			@trending_deal.save!
-  		end
-  		@subscribe_deal = SubscribeDeal.find_by_app_user_id_and_deal_id(params[:app_user_id],params[:deal_id])    
-  		if @subscribe_deal.present? 
-  			if @subscribe_deal.active_status == false
-  				@subscribe_deal.active_status = true
-  				@subscribe_deal.save!
-  				render :status => 200,
-             		 :json => {
-                        		:success => true
-                        	}	
-  			elsif @subscribe_deal.active_status == true
-  				render :status => 200,
-             		 :json => {
-                        		:success => true
-                        	}	
-  			end
-  		else
-  			@subscribe_deal = SubscribeDeal.new(subscribe_deal_params)
-  			@subscribe_deal.active_status = true
-  			@subscribe_deal.category_id = @deal.service_category_id
-  			@subscribe_deal.save!
-  			respond_to do |format|
-      		if @subscribe_deal.save
-        		#send_notification
-        		format.html { redirect_to subscribe_deals_path, :notice => 'Successfully created' }
-        		format.xml  { render :xml => @subscribe_deal, :status => :created, :subscribe_deal => @subscribe_deal }
-      		else
-        		#raise @deal.errors.inspect
-        		format.html { render :action => "new" }
-        		format.json  { render :json => @subscribe_deal.errors, :status => :unprocessable_entity }
-      		end
-    		end
-  		end	
-  	else
-  		render :status => 200,
+    if params[:app_user_id].present? && params[:deal_id].present?
+      @subscribe_deal = SubscribeDeal.find_by_app_user_id_and_deal_id(params[:app_user_id],params[:deal_id])    
+      @deal = Deal.find_by_id(params[:deal_id])
+      if @subscribe_deal.present?
+        if @subscribe_deal.active_status == true
+          render :status => 200,
+                 :json => {
+                            :success => true
+                          } 
+        elsif @subscribe_deal.active_status == false
+          @subscribe_deal.active_status = true
+          @subscribe_deal.save!
+          @trending_deal = TrendingDeal.where("deal_id =?", params[:deal_id]).take
+          if @trending_deal.present?
+            @trending_deal.subscription_count = @trending_deal.subscription_count + 1
+            @trending_deal.save!
+          else
+            @trending_deal = TrendingDeal.new(trending_deal_params)
+            @trending_deal.category_id = @deal.service_category_id
+            @trending_deal.subscription_count = 1
+            @trending_deal.save!
+          end
+        end
+      else
+        @subscribe_deal = SubscribeDeal.new(subscribe_deal_params)
+        @subscribe_deal.active_status = true
+        @subscribe_deal.category_id = @deal.service_category_id
+        @subscribe_deal.save!
+        @trending_deal = TrendingDeal.where("deal_id =?", params[:deal_id]).take
+        if @trending_deal.present?
+          @trending_deal.subscription_count = @trending_deal.subscription_count + 1
+          @trending_deal.save!
+        else
+          @trending_deal = TrendingDeal.new(trending_deal_params)
+          @trending_deal.category_id = @deal.service_category_id
+          @trending_deal.subscription_count = 1
+          @trending_deal.save!
+        end
+        render :status => 200,
+                 :json => {
+                            :success => true
+                          }
+      end
+    else
+      render :status => 401,
              :json => {
                         :success => false
-                      }		    
-  	end
+                      }     
+    end  
   end
+
+  
 
   private
 	def subscribe_deal_params

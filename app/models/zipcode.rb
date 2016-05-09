374efd9ae6de3c9bbca4f2380e3f8044fa374c98
Prafull <pp.pprafull.pp@gmail.com>
2016-05-09 12:20:13 +0530

    Add Equipment,Add Additional Offer & Import Zipcode

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -2,4 +2,22 @@ class Zipcode < ActiveRecord::Base
 	validates_uniqueness_of :code
 	has_and_belongs_to_many :deals
 	has_and_belongs_to_many :additional_offers
+
+	def as_json(opts={})
+    json = super(opts)
+    Hash[*json.map{|k, v| [k, v || ""]}.flatten]
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      zipcode_hash = row.to_hash # exclude the price field
+      zipcode = Zipcode.where(id: zipcode_hash["id"])
+
+      if zipcode.count == 1
+        zipcode.first.update_attributes(zipcode_hash)
+      else
+        Zipcode.create!(zipcode_hash)
+      end # end if !zipcode.nil?
+    end # end CSV.foreach
+  end # end self.import(file)
 end

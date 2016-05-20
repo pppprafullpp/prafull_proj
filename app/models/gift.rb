96ff39a27703e30fadb2c65e9d617b0a959b21f7
Prafull <pp.pprafull.pp@gmail.com>
2016-05-20 18:57:10 +0530

    Removing validation of description from Gift model

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,6 +2,6 @@ class Gift < ActiveRecord::Base
 	has_many :user_gifts,:dependent => :destroy
   	has_many :orders, through: :user_gifts
 
-  	validates_presence_of :name, :description, :amount, :is_active, :activation_count_condition
+  	validates_presence_of :name,:amount, :is_active, :activation_count_condition
   	validates_uniqueness_of :activation_count_condition
 end

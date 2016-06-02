c4ae3f5ead0b8cfbce6576461a7fade43ead8fda
pppprafullpp <pp.pprafull.pp@gmail.com>
2016-06-02 15:44:23 +0530

    Changes in Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   #before_filter :authenticate_user!, :except => [:index]
 
   def index
-    redirect_to new_user_session_path if !user_signed_in? rescue nil
+    # redirect_to new_user_session_path if !user_signed_in? rescue nil
   end
 
 end

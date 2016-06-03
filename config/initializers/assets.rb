bae17e8f82768a87243ed7e319e0cf79e157115e
pppprafullpp <pp.pprafull.pp@gmail.com>
2016-06-03 12:27:43 +0530

    Changes in assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( vendor/modernizr.js )
 # Rails.application.config.assets.precompile += %w( website/* )
+Rails.application.config.assets.precompile += %w( website/bootstrap.min.css )

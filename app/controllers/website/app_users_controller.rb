7881fc59cb0093853e82a2a75092dc5c98306565
Apoorv <ankur@spa-systems.com>
2016-07-20 19:36:36 +0530

    profile page updates

--- app/controllers/website/app_users_controller.rb
+++ app/controllers/website/app_users_controller.rb
@@ -34,12 +34,13 @@ class Website::AppUsersController < ApplicationController
   end
 
   def update
+
     if session[:user_id].present?
       @app_user = AppUser.find(session[:user_id])
-      #address = params[:address].present? ? params[:address] : ''
-      #address1 = params[:address1].present? ? params[:address1] : ''
-      #address2 = params[:address2].present? ? params[:address2] : ''
-      #@app_user.address = address + '===' + address1 + '===' + address2
+      address = params[:address].present? ? params[:address] : ''
+      address1 = params[:address1].present? ? params[:address1] : ''
+      address2 = params[:address2].present? ? params[:address2] : ''
+      @app_user.address = address + '===' + address1 + '===' + address2
       #@app_user.city = params[:city];@app_user.state = params[:state]
       first_name = params[:first_name].present? ? params[:first_name].split(' ')[0] : @app_user.first_name
       last_name = params[:first_name].present? ? params[:first_name].split(' ')[1] : @app_user.last_name
@@ -47,6 +48,7 @@ class Website::AppUsersController < ApplicationController
       @app_user.last_name = last_name.present? ? last_name : " "
       @app_user.mobile = params[:mobile]
       @app_user.user_type = params[:user_type] if params[:user_type].present?
+    
       if @app_user.save!
         if @app_user.user_type == AppUser::BUSINESS
           @business = Business.create_business(params)

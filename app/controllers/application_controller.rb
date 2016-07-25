dc7265cb4def28b4f9f61383a670c26bd05e8f57
Amit Pandey <amit.pandey.iitm@gmail.com>
2016-07-26 01:29:25 +0530

    CR:AMIT PANDEY encrypt data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :null_session
 
-  before_filter do
-  	resource = controller_name.singularize.to_sym
-  	method = "#{resource}_params"
-  	params[resource] &&= send(method) if respond_to?(method, true)
-  	:cors_set_access_control_headers
-  end
+	before_filter do
+		resource = controller_name.singularize.to_sym
+		method = "#{resource}_params"
+		params[resource] &&= send(method) if respond_to?(method, true)
+		:cors_set_access_control_headers
+	end
 
-  def cors_set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-	headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-	headers['Access-Control-Request-Method'] = '*'
-	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
+	def cors_set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	end
+
 end

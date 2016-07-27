58f8772294bbb57d47bb7a5ebc47252f1902ad7c
Apoorv <ankur@spa-systems.com>
2016-07-27 16:10:00 +0530

    updated api

--- app/controllers/api/v1/device_tracker_controller.rb
+++ app/controllers/api/v1/device_tracker_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DeviceTrackerController < ApplicationController
   def add_devise_details
-    device_id=params[:devise_id]
+    device_id=params[:device_id]
     service_provider=params[:service_provider]
     save= DeviceTracker.create!(:device_id=>device_id, :service_provider=>service_provider)
     if save

c458b1f4ff85fbb7ad92ec134576c23f2b0a04d7
apoorv <apoorv.coder@live.com>
2016-08-27 15:46:19 +0530

    effective price issue resolved

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::DashboardsController < ApplicationController
 		elsif params[:app_user_id].present? && params[:zip_code].present? && params[:deal_type].blank? && params[:category].present? && params[:sorting_flag].present?
 
 			allowed_deals=filtered_deals(params[:app_user_id],params[:category],nil,nil,params[:sorting_flag])
-			sponsored_deals = allowed_deals.where(is_sponsored: true)
+ 			sponsored_deals = allowed_deals.where(is_sponsored: true)
 			all_deals = sponsored_deals + (allowed_deals - sponsored_deals)
 			bundle_deals = BundleDealAttribute.get_linked_bundle_deal(params[:category])
 			render :json => {:deal => all_deals.as_json(:except => [:created_at, :updated_at, :image, :price],:methods => [:deal_image_url, :average_rating, :rating_count, :deal_price,:service_category_name, :service_provider_name,:deal_additional_offers,:deal_equipments]),

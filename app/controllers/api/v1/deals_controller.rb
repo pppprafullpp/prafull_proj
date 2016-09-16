e7fa575e3287861f2f983dbdb1c2db3844cfe6c6
pppprafullpp <pp.pprafull.pp@gmail.com>
2016-09-16 18:10:38 +0530

    CR:PRAFULL Combining API for Cellphone and Cable

--- app/controllers/api/v1/deals_controller.rb
+++ app/controllers/api/v1/deals_controller.rb
@@ -89,7 +89,7 @@ class Api::V1::DealsController < ApplicationController
 	end
 
 	def customisable_deals
-		deals = Deal.where('is_customisable =? AND service_category_id=?', true,Deal::CELLPHONE_CATEGORY)
+		deals = Deal.where('is_customisable =?', true)
 		render :json => { :success => true, deals: deals}
 	end
 
@@ -104,11 +104,15 @@ class Api::V1::DealsController < ApplicationController
 	end
 
 	def customisable_deal_deatail
-
 		if params[:deal_id].present?
 			deal = Deal.find(params[:deal_id])
-		
+			if deal.service_category_id == Deal::CABLE_CATEGORY
+				render :json => { :success => true, deals: deals.as_json(:include =>{:channel_packages => {:methods=>[:channel_name]},:deal_attributes => {:methods => [:channel_name]},:deal_extra_services => {:methods => [:service_name,:service_description]}},:except => [:created_at, :updated_at, :image, :price],:methods => [:deal_image_url, :average_rating, :rating_count, :deal_price,:service_category_name, :service_provider_name,:deal_additional_offers,:deal_equipments])}
+			elsif deal.service_category_id == Deal::CELPHONE_CATEGORY
 			 render :json => { :success => true, deals: deal.as_json(:include =>{:deal_equipments =>{:except=>[:available_colors],:methods => [:available_color,:cellphone_name,:brand,:description]},:deal_extra_services => {:methods => [:service_name,:service_description]} },:except => [:created_at, :updated_at, :image, :price],:methods => [:deal_image_url, :average_rating, :rating_count, :deal_price,:service_category_name, :service_provider_name,:deal_additional_offers,:deal_attributes])}
+			else
+				render :json => { :success => false}
+			end
 		else
 			render :json => { :success => false}
 		end

b91a536ba3f3c9eed67fb363219a36f9a33ea632
Amit Pandey <amit.pandey.iitm@gmail.com>
2016-10-23 21:19:37 +0530

    CR:AMIT PANDEY

--- app/models/cellphone_service_preference.rb
+++ app/models/cellphone_service_preference.rb
@@ -6,13 +6,14 @@ class CellphoneServicePreference < ActiveRecord::Base
   end
 
   def self.cellphone_best_deal(service_preference,select_data,deal_validation_conditions,restricted_deals,options= {})
-    # raise options.to_yaml
+    # raise restricted_deals.to_yaml
     best_deals = nil
     existing_domestic_call_unlimited = service_preference.cellphone_service_preference.domestic_call_unlimited
     existing_domestic_call_minutes = (existing_domestic_call_unlimited == true) ? 0 : service_preference.cellphone_service_preference.domestic_call_minutes
     existing_data_plan = service_preference.cellphone_service_preference.data_plan
     existing_no_of_lines = (service_preference.cellphone_service_preference.no_of_lines.present? and service_preference.cellphone_service_preference.no_of_lines > 0) ? service_preference.cellphone_service_preference.no_of_lines : 1
-    no_of_lines_condition = " AND cellphone_deal_attributes.no_of_lines = #{existing_no_of_lines}"
+    #no_of_lines_condition = " AND cellphone_deal_attributes.no_of_lines = #{existing_no_of_lines}"
+    no_of_lines_condition= ''
     sort_by = options['sort_by'].present? ? options['sort_by'] : 'price ASC'
     best_deals = Deal.joins(:cellphone_deal_attributes).select(select_data).where(deal_validation_conditions+" AND cellphone_deal_attributes.data_plan > ? AND cellphone_deal_attributes.domestic_call_minutes='Unlimited' AND cellphone_deal_attributes.effective_price <= ? AND deals.id not in (?) #{no_of_lines_condition}", existing_data_plan,service_preference.price,restricted_deals).order(sort_by)
     if best_deals.blank?
